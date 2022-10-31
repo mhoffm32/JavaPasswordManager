be8d46edfe6e8dd8438242dc922ed8f401b55f92
mhoffm32 <113637769+mhoffm32@users.noreply.github.com>
2022-10-31 17:12:06 -0400

    Add files via upload

---
+++ Password.java
@@ -0,0 +1,167 @@
+package Package1;
+import java.util.HashMap;
+import java.util.Scanner;
+
+//Class to hold app, passwords, entrance password
+public class Password {
+    public static Scanner input = new Scanner(System.in);
+    private HashMap<String,String> PassMap;
+    private String entrancePassword;
+   Password(HashMap<String,String> myPassMap){ //Constructor to make Password obj
+       this.PassMap = myPassMap;
+   }
+    public void setEntrancePassword(String entrancePassword) { //setting entrance password
+        this.entrancePassword = entrancePassword;
+    }
+    public String getEntrancePassword(){ //get entrance pass
+       return entrancePassword;
+    }
+
+    public void changeEntrancePassword(){ //to change password protecting the database
+       boolean same = false;
+       do{
+           System.out.print("Enter your old entrance password: ");
+           //user enters password twice to change it
+           String oP = input.next();
+           if(oP.equals(getEntrancePassword())) {
+               System.out.print("\n");
+               same = true;
+               System.out.println("Correct.");
+               String newP = PasswordSafety();
+               setEntrancePassword(newP); //setting new entrance password
+               System.out.println(getEntrancePassword() + " is your new entrance password.");
+           }
+           else {
+               System.out.println("Incorrect");
+           }
+       }while(!same); //keeps asking for input if you get initial password wrong
+    }
+
+
+    public HashMap<String,String> changePassword(HashMap<String,String> passWords) {
+       //to change a password in hashmap, returns the modified hashmap
+        System.out.print("Enter the application to which you would like to change the password: ");
+        String app1 = input.next();
+
+        if (passWords.containsKey(app1)) {
+            //verifying that the database contains a password for this app
+            passWords.replace(app1,PasswordSafety());
+
+        } else {
+            System.out.println("The password database does not contain data for " +
+                    app1 + ". " + "Type 'A' if you would like to add it.");
+            //allows user to add new Key Value pair if key does not exist
+            String choice = input.nextLine();
+            if (choice.equals("A")){
+                addPassword(passWords);
+            }
+        }
+        return passWords; //returns modified hashmap
+    }
+    public void viewPasswords(HashMap<String,String> passWords){ //prints out the database
+        System.out.println("\n" + passWords);
+        System.out.print("\nTo search for a specific password," +
+                " enter the application you need the password to: ");
+        //allows user to search for a specific password using the corresponding app.
+        String app = input.next();
+        if (passWords.containsKey(app)) {
+            System.out.println("The password for " + app + " is " +
+                    passWords.get(app) + "\n"); //hashmap get method to return corresponsing password
+        } else {
+            System.out.println("The password database does not contain data for " +
+                    app + ". " + "Type 'A' to add a password for " + app);
+            String choice = input.next();
+            if (choice.equals("A")) {
+                addPassword(passWords);
+            }
+        }
+    }
+    public HashMap<String,String> addPassword(HashMap<String,String> passWords) {//takes in hashmap parameter to add passwords to
+        System.out.print("Enter the application: ");
+        String app = input.next();
+        if (passWords.containsKey(app)) { //checks to see if a password for this app exists already
+            System.out.println("You already have a a password stored for " + app);
+        } else {
+            passWords.put(app,PasswordSafety());
+            //if it doesnt already exist, adds a new app password pair to the hashmap
+        }
+        return passWords; //returns the revised database
+    }
+
+    public HashMap<String,String> deletePassword(HashMap<String,String> passWords) {//takes in hashmap parameter to delete passwords from
+        System.out.print("Enter the application you would like to delete data for: ");
+        String app = input.next();
+        if (passWords.containsKey(app)) {//checks to see if data is stored for this key value pair
+            System.out.println("{"+app+"="+passWords.get(app)+"}"); //prints the current pair
+            passWords.remove(app); //deletes the pair
+            System.out.println("\nData for " + app + " has been deleted.");
+        } else {
+            System.out.println("Data for " + app + " does not exist in the database.");
+            //if it doesnt exist in map
+        }
+        return passWords; //returns the revised database
+    }
+
+
+    public String PasswordSafety() { //returns a safe password
+        boolean isSafe = false; //returns true if password meets security criteria
+        String myPassword;
+        do {
+            System.out.print("Enter password: ");
+            myPassword = input.next();
+            boolean digitSafe = true, sCharSafe = true, lengthSafe = true,
+                    lowerCaseSafe = true, upperCaseSafe = true;
+            int upperCase = 0, lowerCase = 0, sCharCount = 0, digitCount = 0;
+
+            for (int i = 0; i < myPassword.length(); i++) { //iterating through characters of password
+                //checking if special char exists;
+                if (!Character.isDigit(myPassword.charAt(i))
+                        && !Character.isLetter(myPassword.charAt(i))
+                        && !Character.isWhitespace(myPassword.charAt(i))) {
+                    sCharCount++; //increases when special char is detected
+                }
+                //numbers increases if password contains digits;
+                if (Character.isDigit(myPassword.charAt(i))) {
+                    digitCount++;
+                }
+                //counts lower case
+                if (Character.isLowerCase(myPassword.charAt(i))) {
+                    lowerCase++;
+                } //counts upper case
+
+                if (Character.isUpperCase(myPassword.charAt(i))) {
+                    upperCase++;
+                }
+            }
+            if (digitCount < 2) { //safe if contains more than 2 digits
+                digitSafe = false;
+                System.out.println("Password must contain at least " + (2 - digitCount) + " more digits.");
+            }
+            if (sCharCount == 0) { //safe if contains a special character
+                sCharSafe = false;
+                System.out.println("Password must contain at least 1 special character.");
+            }
+            if (lowerCase == 0) { //safe if contains at least 1 lowercase
+                lowerCaseSafe = false;
+                System.out.println("Password must contain at least 1 lower case character.");
+            }
+            if (upperCase == 0) { //safe if contains at least 1 lowercase
+                upperCaseSafe = false;
+                System.out.println("Password must contain at least 1 upper case character.");
+            }
+            if (myPassword.length() < 12) { //safe if contains 12 characters or more
+                lengthSafe = false;
+                System.out.println("Password must contain at least 12 characters.");
+            }
+
+            if (digitSafe && sCharSafe && lowerCaseSafe && upperCaseSafe && lengthSafe) {
+                System.out.println("Password is safe. Saved to the database. ");
+                isSafe = true; //loop breaks when password is safe
+            }
+        } while (!isSafe); //asks user for password until its safe
+
+        return myPassword; //returns string of the accepted password.
+    }
+
+
+}
---
+++ PasswordDriver.java
@@ -0,0 +1,166 @@
+package Package1;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PasswordDriver {
+    //entering file paths, and creating file objects to save new and modified key value pairs
+    public static Scanner input = new Scanner(System.in); //to get user input
+    final static String filePath1 = "passwordStorage.txt";
+    final static String filePath2 = "EntrancePassword.txt";
+    public static File file1 = new File(filePath1); //file object
+    public static File file2 = new File(filePath2);
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> passwordDatabase = new HashMap<>();
+        Password p = new Password(passwordDatabase); //Password Object
+        readToFile(passwordDatabase,p); //reads file and writes the hashmap to input saved from previous run
+        boolean allowed; //to check if user is authorized to enter the program
+        boolean terminate = false;
+        System.out.print("\nEnter the Password to the Password database: ");
+        String pass = input.next();
+        if (pass.equals(p.getEntrancePassword())) { //if user input of password is correct
+            allowed = true;
+        } else {
+            allowed = false;
+            System.out.println("Incorrect Password");
+        }
+        if (allowed) { //if authorized
+            do { //user keeps going until done
+                System.out.print("\n");
+                System.out.printf("%30s","HOME"); //Home screen header
+                System.out.println("\n-------------------------------------------------------------");
+                System.out.println("\nType key to select Action:"); //prints all user options
+                System.out.print(""" 
+
+                        A --> Add Password
+                        C --> Change Password
+                        D --> Delete Password
+                        V --> View Passwords
+                        P --> Change entrance Password
+                        T --> Save progress and terminate program
+                        \s""");
+                String index = input.next();
+                switch (index) {
+                    case "A" -> {
+                        System.out.printf("%30s","ADD PASSWORD");
+                        System.out.println("\n-------------------------------------------------------------");
+                        passwordDatabase = p.addPassword(passwordDatabase); //calls p instance of Password with add password method
+                        System.out.println("\n-------------------------------------------------------------");
+                    }
+                    case "D" ->{
+                        System.out.printf("%30s","DELETE PASSWORD");
+                        System.out.println("\n-------------------------------------------------------------");
+                        passwordDatabase = p.deletePassword(passwordDatabase); //calls delete pass method
+                        System.out.println("\n-------------------------------------------------------------");
+                    }
+                    case "C" -> {
+                        System.out.printf("%30s","CHANGE PASSWORD");
+                        System.out.println("\n-------------------------------------------------------------");
+                        passwordDatabase = p.changePassword(passwordDatabase); //calls change pass method
+                        System.out.println("\n-------------------------------------------------------------");
+                    }
+                    case "V" -> { System.out.printf("%30s","VIEW PASSWORDS");
+                        System.out.println("\n-------------------------------------------------------------");
+                        p.viewPasswords(passwordDatabase); //calls view pass method
+                        System.out.println("\n-------------------------------------------------------------");
+                    }
+                    case "P"-> {
+                        System.out.printf("%30s","CHANGE ENTRANCE PASSWORD");
+                        System.out.println("\n-------------------------------------------------------------");
+                        p.changeEntrancePassword(); //calls change enterance password method
+                        System.out.println("\n-------------------------------------------------------------");
+                    }
+                    case "T" -> terminate = true; //if user selects T the progress is saved and program is terminated
+
+                    default -> System.out.println("Invalid input");
+
+                }
+                writeToFile(passwordDatabase,p); //overwrites modified data to files
+            } while (!terminate); //user continues doing actions until they chose to terminate program
+        }
+    }
+
+    public static void writeToFile(HashMap<String, String> passWords, Password p) {
+        //takes in parameters of hashmap and the entrance password
+        try (BufferedWriter bf2 = new BufferedWriter(new FileWriter(file1))) {
+            // iterate map entries
+            for (Map.Entry<String, String> entry : passWords.entrySet()) {
+                //writes values to file seperated by colon
+                bf2.write(entry.getKey() + ":"
+                        + entry.getValue());
+                // new line
+                bf2.newLine();
+            }
+            bf2.flush(); //emptys bf2
+        } catch (IOException e) { //exception
+            e.printStackTrace();
+        }
+        try (BufferedWriter bf2 = new BufferedWriter(new FileWriter(file2))) {  //writes the entrance password
+            bf2.write(p.getEntrancePassword());
+            bf2.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readToFile(HashMap<String, String> passWords, Password p) {
+        //reads file to get saved data from previous run
+        BufferedReader br1 = null; //initialized as null
+        try {
+            // create file object
+            File file1 = new File(filePath1);
+            //BufferedReader object from the File
+            br1 = new BufferedReader(new FileReader(file1));
+            String line;
+
+            // read file line by line
+            while ((line = br1.readLine()) != null) { //checks all lines for data
+                String[] parts = line.split(":");
+                //splits apart the line and turns into array for each Key Value pair
+
+                String app = parts[0].trim(); //arr at 0 is the app
+                String pass = parts[1].trim(); //arr at 1 is the corresponding password
+
+                if (!app.equals("") && !pass.equals("")) //boolean if both are not empty insert to hashmap
+                    passWords.put(app, pass);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the BufferedReader
+            if (br1 != null) {
+                try {
+                    br1.close();
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+            }
+        }
+
+        BufferedReader br2 = null; //Same thing but for the entrance password
+        try {
+
+            File file2 = new File(filePath2);
+            br2 = new BufferedReader(new FileReader(file2));
+            String line = br2.readLine();
+            if(line != null){
+                p.setEntrancePassword(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (br2 != null) {
+                try {
+                    br2.close();
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+            }
+        }
+    }
+}
